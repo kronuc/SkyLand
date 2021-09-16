ee36dfc79d2f7f3f28bb735050dd09029867c028
kronuc <denerator2@gmail.com>
2021-09-16 14:11:32 +0300

    add interface for object that will be responsible for creating, saving and loading game sesions

---
+++ SL.Model/Services/Abstractions/IGameSesionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SL.Model.Services.Abstractions
+{
+    public interface IGameSesionService<TKey>
+    {
+        TKey CreateGameSesion(GameSesionSettings settings);
+        List<TKey> GetListOfSessionAvailableForLoading();
+        bool CanLoadSesion(TKey id);
+        void LoadGameSession(TKey id);
+        void SaveGameSession(TKey id);
+    }
+}
